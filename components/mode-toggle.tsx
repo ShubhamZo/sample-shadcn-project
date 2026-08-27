c91a07fa46f6eebb50639fb73a7a65bc19e22317
Shubham33 <Shubham@zowork.com>
2026-08-27 15:04:10 +0530

    toggle-theme: Light and Dark

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuCheckboxItem checked={theme === "light"} onCheckedChange={() => setTheme("light")}>
+          Light
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === "dark"} onCheckedChange={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === "system"} onCheckedChange={() => setTheme("system")}>
+          System
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ data/dashboard-data.ts
@@ -0,0 +1,63 @@
+export type AppointmentType = "Follow-up" | "Consultation" | "Check-up";
+export type AppointmentStatusType = "confirmed" | "waiting" | "completed";
+export type patientGenderType = "Male" | "Female";
+export type lastVisitStatusType = "Stable" | "Follow_up";
+
+export const AppointmentDetails: {
+    patientId: string;
+    patient: string;
+    patientInitials: string;
+    age: number;
+    gender?: patientGenderType;
+    visitingTime?: string;
+    visitingType: AppointmentType;
+    Status: AppointmentStatusType;
+    lastVisitDate?: string;
+    lastVisitStatus?: lastVisitStatusType;
+    avatarColor: "blue" | "green" | "purple" | "red";
+  }[]
+    = [
+      { patientId: "PT-10245", 
+        patient: "Emily Johnson", 
+        patientInitials: "EJ", 
+        age: 32, 
+        gender: "Female", 
+        visitingTime: "09:30 AM", 
+        visitingType: "Follow-up", 
+        Status: "confirmed", 
+        lastVisitDate: "Aug 18", 
+        lastVisitStatus: "Stable" ,
+        avatarColor: "blue"
+    },
+      { patientId: "PT-10312", 
+        patient: "Michael Brown", 
+        patientInitials: "MB", 
+        age: 45, gender: "Male", 
+        visitingTime: "10:30 AM", 
+        visitingType: "Consultation", 
+        Status: "waiting", 
+        lastVisitDate: "Aug 31", 
+        lastVisitStatus: "Follow_up",
+        avatarColor: "green" 
+    },
+      { patientId: "PT-10198", 
+        patient: "Olivia Davis", 
+        patientInitials: "OD", 
+        age: 28, gender: "Male", 
+        visitingTime: "11:15 AM", 
+        visitingType: "Check-up", 
+        Status: "confirmed", 
+        lastVisitDate: "Dec 17", 
+        lastVisitStatus: "Stable",
+        avatarColor: "purple" 
+    },
+      { patientId: "PT-10101", 
+        patient: "James Wilson", 
+        patientInitials: "JW", 
+        age: 35, gender: "Female", 
+        visitingTime: "11:45 AM", 
+        visitingType: "Follow-up", 
+        Status: "completed",
+        avatarColor: "red"
+    },
+    ];
